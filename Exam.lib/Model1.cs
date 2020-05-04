29e777b8b722a848dc51bea28b04d319db6ee606
MarkKuklin <55387042+MarkKuklin@users.noreply.github.com>
2020-05-04 17:36:38 +0600

    Add files via upload

---
+++ Exam.lib/Model1.cs
@@ -0,0 +1,34 @@
+﻿namespace Exam.lib
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    public class Model1 : DbContext
+    {
+        // Контекст настроен для использования строки подключения "Model1" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "Exam.lib.Model1" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "Model1" 
+        // в файле конфигурации приложения.
+        public Model1()
+            : base("name=Model1")
+        {
+
+        }
+        public virtual DbSet<Rate> Rates { get; set; }
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // 
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
